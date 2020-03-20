137cb0661b792bb9d0193ea75a0419aec284b6fb
damo <the.secret.lyrics.20316143@gmail.com>
2020-03-21 00:27:19 +0900

    微修正

--- index_table.js
+++ index_table.js
@@ -5,7 +5,7 @@ function makeTable(data, tableId) {
 	var table = document.createElement("table");
 	// tableにid付与
 	table.setAttribute("id", "sampleTable");
-	// 表に2次元配列の要素を格納
+	// 表に配列の要素を格納
 	for (i = 0; i < data.length; i++) {
 		rows.push(table.insertRow(-1)); // 行の追加
 		for (j = 0; j < data[0].length; j++) {
@@ -13,11 +13,11 @@ function makeTable(data, tableId) {
 			// HTML形式で文字列を挿入
 			cell.innerHTML = data[i][j];
 			// 背景色の設定
-						if (i == 0) {
-							cell.style.backgroundColor = "#bbb"; // ヘッダ行
-						} else {
-							cell.style.backgroundColor = "#ddd"; // ヘッダ行以外
-						}
+			if (i == 0) {
+				cell.style.backgroundColor = "#bbb"; // ヘッダ行
+			} else {
+				cell.style.backgroundColor = "#ddd"; // ヘッダ行以外
+			}
 			if (i == 0) {
 				cell.classList.add('cmanSortBtn');
 			}
@@ -198,6 +198,46 @@ window.addEventListener('load', function () {
     ["", "", "", ""],
     ["", "", "", ""],
     ["", "", "", ""],
+
+    /*薄明の翼*/
+    ["DNoM hqlJNo", "", "ダンデのポスター上", "薄明の翼1話"],
+    ["Jq? OrfkoM", "", "ダンデのポスター下", "薄明の翼1話"],
+    ["BEKCVE", "", "ダンデのポスター2", "薄明の翼1話"],
+    ["GALAJ", "", "ダンデのポスター2", "薄明の翼1話"],
+    ["Dfkcq", "", "ダンデのポスター2", "薄明の翼1話"],
+    ["Xh Rlq Kgcrmfki", "", "飲み物のポスター", "薄明の翼1話"],
+    ["ROSE", "ROSE", "ローズのサイン", "薄明の翼1話"],
+
+    ["Ucidkaomhq", "", "赤い本", "薄明の翼2話"],
+    ["Nhgaidomrq", "", "緑の本", "薄明の翼2話"],
+    ["Aagolnhd iml croh Ugqnc", "", "緑の本サブタイトル", "薄明の翼2話"],
+    ["eqo", "", "水色のファイル", "薄明の翼2話"],
+    ["Haqkrhdakm", "", "青の本", "薄明の翼2話"],
+    ["Edi al", "", "青の本サブタイトル", "薄明の翼2話"],
+    ["Ofkcrnik", "", "ワンリキー<br>チョコバー", "薄明の翼2話"],
+    ["Agfrlkolai", "", "ワンリキー<br>チョコバー下段", "薄明の翼2話"],
+
+		["SAFJU", "SAITO", "サイトウ", "薄明の翼3話"],
+    ["Haqkrhdakm", "", "左2段目", "薄明の翼3話"],
+    ["TRNBOK VXDA", "", "左3段目", "薄明の翼3話"],
+    ["DCVDB", "DANDE", "ダンデ", "薄明の翼3話"],
+    ["Nhgaidonrq", "", "右2段目", "薄明の翼3話"],
+    ["Whqdglon", "", "右3段目", "薄明の翼3話"],
+    ["GALAJ", "GALAR", "ガラル", "薄明の翼3話"],
+    ["RLDEJIM", "", "中央下", "薄明の翼3話"],
+    ["ZCISFHJBhLL", "MONSTER BALL", "モンスターボールのポスター", "薄明の翼3話"],
+    ["RE", "", "ポスターの右上", "薄明の翼3話"],
+    ["WeUTCEKV", "", "ブティック", "薄明の翼3話"],
+    ["lfd q lfd", "エイト o エイト", "きのみ屋", "薄明の翼3話"],
+    ["OoRq", "", "垂れ幕", "薄明の翼3話"],
+    ["EUT? DUeXE", "", "美容院", "薄明の翼3話"],
+    ["OoRq", "", "迷子札", "薄明の翼3話"],
+    ["CFSR VXDA", "", "迷子札<br>ハロン", "薄明の翼3話"],
+    ["FeeKU JVYU", "", "迷子札", "薄明の翼3話"],
+    ["EOU", "HOP", "迷子札<br>ホップ", "薄明の翼3話"],
+    ["", "", "", "薄明の翼3話"],
+
+
 		/*文章*/
 		["XBeSVHeR", "", "賞状のとこ", "賞状"],
 		["AKFMVSL", "", "プレイヤーのとこ", "賞状"],
@@ -713,4 +753,199 @@ window.addEventListener('load', function () {
 
 	// 表の動的作成
 	makeTable(data, "table");
+	tSortInit();
 });
+
+
+//< script type = "text/javascript" >
+//===============================================================
+//  テーブルソートの共通変数　設定要！
+//===============================================================
+var gTabldID = 'sampleTable'; // テーブルのエリアのIDを設定
+var gSortNum = 1; // 1 : 数値に見える列は数値でソート
+var gSortAa = 1; // 1 : 英大文字と小文字を区別しない
+
+var gSortBtnRow = 0;
+
+//===============================================================
+//  テーブルの初期設定
+//===============================================================
+function tSortInit() {
+
+	var wTABLE = document.getElementById(gTabldID);
+	var wTR = wTABLE.rows;
+	var wAddBtn = '';
+
+	// --- テーブル内を検索してソートボタンを付ける ----------------
+	for (var i = 0; i < wTR.length; i++) {
+
+		var wTD = wTABLE.rows[i].cells;
+		for (var j = 0; j < wTD.length; j++) {
+
+			if (wTD[j].getAttribute('cmanSortBtn') !== null) {
+
+				wAddBtn = '<div class="tsImgArea">';
+				wAddBtn += '<svg class="tsImg" id="ts_A_' + j + '" onclick="tSort(this)"><path d="M4 0 L0 6 L8 6 Z"></path></svg>';
+				wAddBtn += '<svg class="tsImg" id="ts_D_' + j + '" onclick="tSort(this)"><path d="M0 0 L8 0 L4 7 Z"></path></svg>';
+				wAddBtn += '</div>';
+
+				wTD[j].innerHTML = wTD[j].innerHTML + wAddBtn;
+			}
+		}
+
+		// --- ボタンを付けたら以降の行は無視する --------------------
+		if (wAddBtn != '') {
+			gSortBtnRow = i;
+			break;
+		}
+	}
+}
+
+//===============================================================
+//  ソート実行
+//===============================================================
+function tSort(argObj) {
+
+	// 「ts_A_1」形式 [1]:A-昇順,D-降順  [2]:列番号
+	var wSortKey = argObj.id.split('_');
+
+	var wTABLE = document.getElementById(gTabldID);
+	var wTR = wTABLE.rows;
+	var wItem = []; // クリックされた列の値
+	var wItemSort = []; // クリックされた列の値（項目ソート後）
+	var wMoveRow = []; // 元の行位置（行削除考慮位置）
+	var wNotNum = 0; // 1 : 数字でない
+	var wStartRow = gSortBtnRow + 1; // ソートを開始する行はボタンの次の行
+
+	// ------------------------------------------------------
+	//  クリックされた列の値を取得する
+	// ------------------------------------------------------
+	for (var i = wStartRow; i < wTR.length; i++) {
+		var j = i - wStartRow;
+		wItem[j] = wTR[i].cells[wSortKey[2]].innerText.toString();
+
+		if (wItem[j].match(/^[-]?[0-9,\.]+$/)) {} else {
+			wNotNum = 1;
+		}
+
+	}
+	// ソート用に配列をコピー
+	wItemSort = wItem.slice(0, wItem.length);
+
+	// ------------------------------------------------------
+	//  列の値でソートを実行
+	// ------------------------------------------------------
+	if (wSortKey[1] == 'A') {
+		if ((gSortNum == 1) && (wNotNum == 0)) {
+			wItemSort.sort(sortNumA); // 数値で昇順
+		} else {
+			wItemSort.sort(sortStrA); // 文字で昇順
+		}
+	} else {
+		if ((gSortNum == 1) && (wNotNum == 0)) {
+			wItemSort.sort(sortNumD); // 数値で降順
+		} else {
+			wItemSort.sort(sortStrD); // 文字で降順
+		}
+	}
+
+	// ------------------------------------------------------
+	//  行の入れ替え順を取得
+	//    ソート前後の列の値を比較して行の移動順を確定
+	//    配列を削除して前詰めしている（移動時も同じ動き）
+	// ------------------------------------------------------
+	for (var i = 0; i < wItemSort.length; i++) {
+		for (var j = 0; j < wItem.length; j++) {
+			if (wItemSort[i] == wItem[j]) {
+				wMoveRow[i] = j + wStartRow;
+				wItem.splice(j, 1);
+				break;
+			}
+		}
+	}
+
+	// ------------------------------------------------------
+	//  ソート順に行を移動
+	// ------------------------------------------------------
+	for (var i = 0; i < wMoveRow.length; i++) {
+
+
+		var wMoveTr = wTABLE.rows[wMoveRow[i]]; // 移動対象
+		var wLastTr = wTABLE.rows[wTABLE.rows.length - 1]; // 最終行
+
+		// 最終行にコピーしてから移動元を削除
+		wLastTr.parentNode.insertBefore(wMoveTr.cloneNode(true), wLastTr.nextSibling);
+		wTABLE.deleteRow(wMoveRow[i]);
+
+	}
+
+	// ------------------------------------------------------
+	//  クリックされたソートボタンの色付け
+	// ------------------------------------------------------
+	var elmImg = document.getElementsByClassName('tsImg');
+	for (var i = 0; i < elmImg.length; i++) {
+
+		if (elmImg[i].id == argObj.id) {
+			elmImg[i].style.backgroundColor = '#ffff00';
+		} else {
+			elmImg[i].style.backgroundColor = '';
+		}
+
+	}
+
+}
+
+//===============================================================
+//  数字のソート関数（昇順）
+//===============================================================
+function sortNumA(a, b) {
+	a = parseInt(a.replace(/,/g, ''));
+	b = parseInt(b.replace(/,/g, ''));
+	return a - b;
+}
+
+//===============================================================
+//  数字のソート関数（降順）
+//===============================================================
+function sortNumD(a, b) {
+	a = parseInt(a.replace(/,/g, ''));
+	b = parseInt(b.replace(/,/g, ''));
+	return b - a;
+}
+
+//===============================================================
+//  文字のソート関数（昇順）
+//===============================================================
+function sortStrA(a, b) {
+	a = a.toString();
+	b = b.toString();
+	if (gSortAa == 1) { // 1 : 英大文字小文字を区別しない
+		a = a.toLowerCase();
+		b = b.toLowerCase();
+	}
+	if (a < b) {
+		return -1;
+	} else if (a > b) {
+		return 1;
+	}
+	return 0;
+}
+
+//===============================================================
+//  文字のソート関数（降順）
+//===============================================================
+function sortStrD(a, b) {
+	a = a.toString();
+	b = b.toString();
+	if (gSortAa == 1) { // 1 : 英大文字小文字を区別しない
+		a = a.toLowerCase();
+		b = b.toLowerCase();
+	}
+
+	if (b < a) {
+		return -1;
+	} else if (b > a) {
+		return 1;
+	}
+	return 0;
+}
